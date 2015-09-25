8cbd50596686be7c93e8d69e99b1bca3542ff11f
lixiangzhong <361293862@qq.com>
2015-09-25 08:42:09 +0800

    GBK<->UTF8

--- character.go
+++ character.go
@@ -7,3 +7,6 @@ import (
 func GBKtoUTF8(gbk string) string {
 	return mahonia.NewDecoder("GB18030").ConvertString(gbk)
 }
+func UTF8toGBK(utf8 string) string {
+	return mahonia.NewEncoder("GB18030").ConvertString(utf8)
+}
