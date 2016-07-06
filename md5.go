6ba185f74f7a795d170b121195fc42d0c47c0f86
lxz@dns <lxz@dns>
2016-07-06 10:40:17 +0800

    MD5WithSalt()

--- md5.go
+++ md5.go
@@ -10,3 +10,12 @@ func MD5(s string) string {
 	m.Write([]byte(s))
 	return hex.EncodeToString(m.Sum(nil))
 }
+
+func MD5WithSalt(s string, salts ...string) string {
+	m := md5.New()
+	m.Write([]byte(s))
+	for _, v := range salts {
+		m.Write([]byte(v))
+	}
+	return hex.EncodeToString(m.Sum(nil))
+}
