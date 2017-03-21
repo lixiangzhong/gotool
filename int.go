36e5d654f7e451af07730eb0540ded1114fc3499
lxz@mibook <361293862@qq.com>
2017-03-21 16:04:24 +0800

    Comma() 将数字转成逗号分隔形式便于阅读,如 1000 转成 1,000

---
+++ int.go
@@ -0,0 +1,37 @@
+package gotool
+
+import (
+	"math"
+	"strconv"
+	"strings"
+)
+
+//将数字转成逗号分隔形式便于阅读,如 1000 转成 1,000
+func Comma(v int64) string {
+	sign := ""
+	if v == math.MinInt64 {
+		return "-9,223,372,036,854,775,808"
+	}
+
+	if v < 0 {
+		sign = "-"
+		v = 0 - v
+	}
+
+	parts := []string{"", "", "", "", "", "", ""}
+	j := len(parts) - 1
+
+	for v > 999 {
+		parts[j] = strconv.FormatInt(v%1000, 10)
+		switch len(parts[j]) {
+		case 2:
+			parts[j] = "0" + parts[j]
+		case 1:
+			parts[j] = "00" + parts[j]
+		}
+		v = v / 1000
+		j--
+	}
+	parts[j] = strconv.Itoa(int(v))
+	return sign + strings.Join(parts[j:], ",")
+}
